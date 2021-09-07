790d548a4164491a487edca53ea06dfbd864777b
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-09-07 17:57:44 -0600

    Two pages were added. Login.vue and Register.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Login page</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class LoginPage extends Vue {}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Register page</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class RegisterPage extends Vue {}
+</script>
