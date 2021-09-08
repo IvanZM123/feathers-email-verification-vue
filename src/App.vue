195b66e7660577d233680ee5c05bc2beb28d2e08
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-09-07 20:59:47 -0600

    Navbar was added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-light border-0 shadow py-2"
+    >
+      <div class="container-fluid">
+        <router-link :to="{ name: 'Home' }" class="text-decoration-none">
+          <a class="navbar-brand" href="#">Feathers</a>
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'Login' }">
+                <button class="btn border-0">Login</button>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'Register' }" class="nav-link">
+                <button class="btn btn-primary border-0 py-2 px-3">
+                  Create account
+                </button>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
-  <router-view />
+
+  <main>
+    <div class="container-fluid py-2">
+      <router-view />
+    </div>
+  </main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+@Options({})
+export default class App extends Vue {}
+</script>
