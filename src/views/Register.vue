d5f4a062bd9952e239b4de2e12d716424706618e
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-09-07 21:18:43 -0600

    Styles added to Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,60 @@
 <template>
-  <h1>Register page</h1>
+  <div class="container-fluid">
+    <div
+      class="row justify-content-center align-items-center"
+      style="min-height: 85vh"
+    >
+      <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+        <div class="card bg-white border-0 shadow p-2">
+          <div class="card-body">
+            <form>
+              <div class="form-group py-2">
+                <label for="fullname">Fullname</label>
+                <input
+                  placeholder="Exp: Ivan Zaldivar"
+                  class="form-control my-1"
+                  autocomplete="off"
+                  name="fullname"
+                  id="fullname"
+                  autofocus
+                  required
+                />
+                <small class="form-text text-muted">Enter your name</small>
+              </div>
+              <div class="form-group py-2">
+                <label for="email">Email</label>
+                <input
+                  placeholder="Exp: abc@gmail.com"
+                  class="form-control my-1"
+                  autocomplete="off"
+                  name="email"
+                  id="email"
+                  required
+                />
+                <small class="form-text text-muted">Enter your email</small>
+              </div>
+              <div class="form-group py-2">
+                <label for="password">Password</label>
+                <input
+                  class="form-control my-1"
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
+                />
+                <small class="form-text text-muted">Enter your password</small>
+              </div>
+            </form>
+            <div class="pt-2">
+              <button class="btn btn-primary border-0 py-2 px-3 w-100">
+                Create account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
